336dcd2ee12e582e52bacec52af4ea9a0f525330
cristobal <cristobal@jotaa.cl>
2023-07-22 17:06:20 -0400

    detalles de estación arreglado

--- src/pages/DetallesEstacion.jsx
+++ src/pages/DetallesEstacion.jsx
@@ -48,45 +48,43 @@ const DetallesEstacion = () => {
                 stationFound && (
                     <Helmet title={`Estación ${stationFound.estacion.nombreEstacion}`}>
                         <div className="container mt-5">
-                            <h2 className={`py-4 text-center ${stationFound.classLinea}`}><strong>{stationFound.estacion.nombreEstacion} <span className={`rounded-circle ${stationFound.classLinea}`}>{stationFound.nombreLinea2}</span></strong></h2>
+                            <div className='mb-2 text-center lineSymbolMobile_container'>
+                                <span className={`rounded-circle detallesEstacion_lineSymbol ${stationFound.classLinea}`}>{stationFound.nombreLinea2}</span>
+                            </div>
+                            <h2 className={`text-center pt-lg-5 ${stationFound.classLinea}`}><strong>  {stationFound.estacion.nombreEstacion} <span className={`rounded-circle lineSymbolDesktop_container ${stationFound.classLinea}`}>{stationFound.nombreLinea2}</span></strong></h2>
 
-                            <p className='mt-4 text-center'><span className={`${stationFound.classLinea}`}><strong><FontAwesomeIcon size="lg" icon={faLocationDot} /> Ubicación:</strong></span> {stationFound.estacion.ubicacion}</p>
+                            <p className='mt-4 text-center detallesEstacion_combinacion'>
+                                <strong className={`${stationFound.classLinea}`}> 
+                                    {stationFound.estacion.combinaciones.length > 1 ? "Combinaciones" : "Combinación"}:
+                                </strong>
+                                <br className='lineSymbolMobile_container' />
+                                {stationFound.estacion.combinaciones.map((combinacion,index) =>(
+                                    <span className={`ms-1 combination_symbol ${combinacion.classCombinacion}`} key={index}>{combinacion.nombreCombinacion} <span className='rounded-circle'>{combinacion.nombreCombinacion2}</span></span> 
+                                ))}
+                                
+                            </p>
 
                             <div className={`mt-5 row justify-content-center`}>
 
-
-                                {/* Combinaciones */}
-                                <div className={`col-12 col-sm-12 col-lg-6 mb-5 ${stationFound.estacion.combinaciones.length >= 1 ? "" : "d-none"}`}>
-                                    <h3 className={`text-center mb-3 mb-lg-5 ${stationFound.classLinea}`}><strong><FontAwesomeIcon size="lg" icon={faTrainSubway} /> {stationFound.estacion.combinaciones.length > 1 ? "Combinaciones" : "Combinación"}</strong></h3>
-
-                                    <ul className='ul_combinations px-0'>
-                                        {stationFound.estacion.combinaciones.map((combinacion,index) =>(
-                                            <li className={`text-center ${combinacion.classCombinacion}`} key={index}>{combinacion.nombreCombinacion} <span className='rounded-circle color_combinacion'>{combinacion.nombreCombinacion2}</span></li>
-                                        ))}
-                                    </ul>
-                                </div>
-
-
                                 {/* Comunas */}
-                                <div className='col-12 col-sm-12 col-lg-6 mb-5'>
+                                <div className='col-12 col-sm-12 col-lg-12 mb-5'>
                                     <h3 className={`text-center mb-3 mb-lg-5 ${stationFound.classLinea}`}><strong><FontAwesomeIcon size="lg" icon={faCity} /> {stationFound.estacion.comunas.length === 1 ? "Comuna" : "Comunas"}</strong></h3>
 
                                     <div className={`mt-3 row justify-content-center`}>
                                         {stationFound.estacion.comunas.map((comuna,index) => (
-                                            <div key={index} className='col-12 col-sm-12 col-md-6 mb-3 mb-sm-5'>
-                                                <h4 className='text-center'>{comuna}</h4>
+                                            <div key={index} className='col-12 col-sm-12 col-md-6 col-lg-4 mb-3 mb-sm-5'>
+                                                <h4 className={`text-center ${stationFound.classLinea}`}><strong>{comuna}</strong></h4>
 
                                                 <div id={`carouselExampleAutoplaying_${index}`} className="carousel slide" data-bs-ride="carousel">
                                                     <div className="carousel-inner">
                                                         
                                                         <div className="carousel-item text-center active">
-                                                            <img className='imgSlideDatailtsStation' src={imgs.filter((comunaImg) => comunaImg.name === comuna)[0].img[1]} alt={comuna} />
+                                                            <img className={`rounded-2 imgSlideDatailtsStation`} src={imgs.filter((comunaImg) => comunaImg.name === comuna)[0].img[1]} alt={comuna} />
                                                         </div>
                                                         <div className="carousel-item text-center">
-                                                            <img className='imgSlideDatailtsStation' src={imgs.filter((comunaImg) => comunaImg.name === comuna)[0].img[0]} alt={comuna} />
+                                                            <img className={`rounded-2 imgSlideDatailtsStation`} src={imgs.filter((comunaImg) => comunaImg.name === comuna)[0].img[0]} alt={comuna} />
                                                         </div>
-                                                        
-                                                        
+                                                          
                                                     </div>
                                                     <button className="carousel-control-prev" type="button" data-bs-target={`#carouselExampleAutoplaying_${index}`} data-bs-slide="prev">
                                                         <FontAwesomeIcon className={`${stationFound.classLinea}`} size="xl" icon={faChevronLeft} />
@@ -110,6 +108,7 @@ const DetallesEstacion = () => {
 
                             {stationFound.estacion.mapa && (
                                 <div className='text-center'>
+                                    <h3 className={`${stationFound.classLinea}`}><strong><FontAwesomeIcon size="xl" icon={faLocationDot} /> Ubicación:</strong></h3>
                                     <iframe
                                         className={`rounded-3 ${stationFound.classLinea}`}
                                         src={stationFound.estacion.mapa}
